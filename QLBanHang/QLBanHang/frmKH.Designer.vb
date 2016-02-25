e6d4cc68eed032014e76e0d6bd84ffae3ae89dc5
JinPoly <phamkiller96@gmail.com>
2016-02-25 10:06:32 +0700

    QuanLyBanHang

---
+++ QLBanHang/QLBanHang/frmKH.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmKH
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtMaKH = New System.Windows.Forms.TextBox()
+        Me.txtTen = New System.Windows.Forms.TextBox()
+        Me.txtDiaChi = New System.Windows.Forms.TextBox()
+        Me.txtSDT = New System.Windows.Forms.TextBox()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "MaKH"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(27, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "TenKH"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(27, 111)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "DiaChi"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(27, 145)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(29, 13)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "SDT"
+        '
+        'txtMaKH
+        '
+        Me.txtMaKH.Location = New System.Drawing.Point(89, 33)
+        Me.txtMaKH.Name = "txtMaKH"
+        Me.txtMaKH.Size = New System.Drawing.Size(100, 20)
+        Me.txtMaKH.TabIndex = 1
+        '
+        'txtTen
+        '
+        Me.txtTen.Location = New System.Drawing.Point(89, 70)
+        Me.txtTen.Name = "txtTen"
+        Me.txtTen.Size = New System.Drawing.Size(100, 20)
+        Me.txtTen.TabIndex = 1
+        '
+        'txtDiaChi
+        '
+        Me.txtDiaChi.Location = New System.Drawing.Point(89, 111)
+        Me.txtDiaChi.Name = "txtDiaChi"
+        Me.txtDiaChi.Size = New System.Drawing.Size(100, 20)
+        Me.txtDiaChi.TabIndex = 1
+        '
+        'txtSDT
+        '
+        Me.txtSDT.Location = New System.Drawing.Point(89, 145)
+        Me.txtSDT.Name = "txtSDT"
+        Me.txtSDT.Size = New System.Drawing.Size(100, 20)
+        Me.txtSDT.TabIndex = 1
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(26, 202)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(57, 23)
+        Me.btnOK.TabIndex = 2
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(132, 202)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(57, 23)
+        Me.btnCancel.TabIndex = 2
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'frmKH
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.QLBanHang.My.Resources.Resources.SinhVienIT_Net___cong_nghe_quai_nhan_cong_nghe_145_jpg
+        Me.ClientSize = New System.Drawing.Size(228, 243)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.txtSDT)
+        Me.Controls.Add(Me.txtDiaChi)
+        Me.Controls.Add(Me.txtTen)
+        Me.Controls.Add(Me.txtMaKH)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmKH"
+        Me.Text = "Khách Hàng"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtMaKH As System.Windows.Forms.TextBox
+    Friend WithEvents txtTen As System.Windows.Forms.TextBox
+    Friend WithEvents txtDiaChi As System.Windows.Forms.TextBox
+    Friend WithEvents txtSDT As System.Windows.Forms.TextBox
+    Friend WithEvents btnOK As System.Windows.Forms.Button
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+End Class
---
+++ QLBanHang/QLBanHang/frmQLBH.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmQLBH
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim Label1 As System.Windows.Forms.Label
+        Me.dgvKH = New System.Windows.Forms.DataGridView()
+        Me.cboSearch = New System.Windows.Forms.ComboBox()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Label1 = New System.Windows.Forms.Label()
+        CType(Me.dgvKH, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvKH
+        '
+        Me.dgvKH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvKH.Location = New System.Drawing.Point(9, 65)
+        Me.dgvKH.MultiSelect = False
+        Me.dgvKH.Name = "dgvKH"
+        Me.dgvKH.ReadOnly = True
+        Me.dgvKH.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvKH.Size = New System.Drawing.Size(445, 251)
+        Me.dgvKH.TabIndex = 0
+        '
+        'Label1
+        '
+        Label1.AutoSize = True
+        Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(163, Byte))
+        Label1.ForeColor = System.Drawing.Color.Blue
+        Label1.Location = New System.Drawing.Point(12, 39)
+        Label1.Name = "Label1"
+        Label1.Size = New System.Drawing.Size(51, 13)
+        Label1.TabIndex = 1
+        Label1.Text = "Search:"
+        '
+        'cboSearch
+        '
+        Me.cboSearch.FormattingEnabled = True
+        Me.cboSearch.Items.AddRange(New Object() {"MaKH", "TenKH"})
+        Me.cboSearch.Location = New System.Drawing.Point(69, 36)
+        Me.cboSearch.Name = "cboSearch"
+        Me.cboSearch.Size = New System.Drawing.Size(98, 21)
+        Me.cboSearch.TabIndex = 2
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(173, 37)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(281, 20)
+        Me.txtSearch.TabIndex = 3
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(460, 64)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(97, 23)
+        Me.btnAdd.TabIndex = 4
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Location = New System.Drawing.Point(460, 93)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(97, 23)
+        Me.btnEdit.TabIndex = 4
+        Me.btnEdit.Text = "Edit"
+        Me.btnEdit.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(460, 122)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(97, 23)
+        Me.btnDelete.TabIndex = 4
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'frmQLBH
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.QLBanHang.My.Resources.Resources.Gk5yV
+        Me.ClientSize = New System.Drawing.Size(563, 334)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnEdit)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.cboSearch)
+        Me.Controls.Add(Label1)
+        Me.Controls.Add(Me.dgvKH)
+        Me.Name = "frmQLBH"
+        Me.Text = "Quản lý bán hàng"
+        CType(Me.dgvKH, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents dgvKH As System.Windows.Forms.DataGridView
+    Friend WithEvents cboSearch As System.Windows.Forms.ComboBox
+    Friend WithEvents txtSearch As System.Windows.Forms.TextBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnEdit As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+
+End Class
